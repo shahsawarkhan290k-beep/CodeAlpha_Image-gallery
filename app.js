87054bd9fae76671a5aab5bd4c696262654a7ba3
shahsawarkhan290k-beep <shahsawarkhan290k@gmail.com>
2025-12-17 10:42:08 +0500

    finalize html and JS logic

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const fullview=document.querySelector(".fullimageshow");
+const images=document.querySelectorAll(".images img");
 const fullimage=document.getElementById("FullImage");
 const nextbtn=document.getElementById("next");
 const prevbtn=document.getElementById("prev");
 const closebtn=document.getElementById("close");
-const slider=document.querySelector(".slider");
+let currpic=0;
 function FullView(ImgLink){
   fullimage.src=ImgLink;
 fullview.style.display="block";
@@ -11,5 +12,23 @@ fullview.style.display="block";
 closebtn.addEventListener("click",()=>{
   fullview.style.display="none";
 });
-
+  images.forEach((img,curr)=>{
+      if(fullimage.src===currpic){
+currpic=curr;
+      }
+  });
+nextbtn.onclick = function () {
+  currpic++;
+  if (currpic >= images.length) {
+    currpic = 0; 
+  }
+  fullimage.src = images[currpic].src;
+};
+prevbtn.onclick=function (){
+  currpic--;
+  if(currpic>=images.length){
+    currpic=0;    
+  }
+  fullimage.src=images[currpic].src;
+};
 
